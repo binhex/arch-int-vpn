07d1afda6938cebb1c2447e5b18f963a5c0367e2
binhex <megalith01@gmail.com>
2015-07-21 18:06:20 +0100

    add in braces to prevent any ambiguous cases for start script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # if vpn set to "no" then don't run openvpn
-if [[ $VPN_ENABLED == "no" ]]; then
+if [[ "${VPN_ENABLED}" == "no" ]]; then
 
 	echo "[info] VPN not enabled, skipping configuration of OpenVPN"
 
@@ -20,21 +20,21 @@ else
 		echo "[crit] VPN provider not defined, please specify via env variable VPN_PROV" && exit 1
 
 	# if airvpn vpn provider chosen then do NOT copy base config file
-	elif [[ $VPN_PROV == "airvpn" ]]; then
+	elif [[ "${VPN_PROV}" == "airvpn" ]]; then
 
-		echo "[info] VPN provider defined as $VPN_PROV"
+		echo "[info] VPN provider defined as ${VPN_PROV}"
 		
 		if [[ -z "${VPN_CONFIG}" ]]; then
 			echo "[crit] Missing OpenVPN configuration file in /config/openvpn/ (no files with an ovpn extension exist) please create and restart delugevpn" && exit 1
 		fi
 
 	# if pia vpn provider chosen then copy base config file and pia certs
-	elif [[ $VPN_PROV == "pia" ]]; then
+	elif [[ "${VPN_PROV}" == "pia" ]]; then
 
-		echo "[info] VPN provider defined as $VPN_PROV"
+		echo "[info] VPN provider defined as ${VPN_PROV}"
 		
 		# copy default certs
-		echo "[info] VPN provider defined as $VPN_PROV"
+		echo "[info] VPN provider defined as ${VPN_PROV}"
 		cp -f /home/nobody/ca.crt /config/openvpn/ca.crt
 		cp -f /home/nobody/crl.pem /config/openvpn/crl.pem
 
@@ -47,27 +47,27 @@ else
 		# if no remote gateway or port specified then use netherlands and default port
 		if [[ -z "${VPN_REMOTE}" && -z "${VPN_PORT}" ]]; then
 			echo "[warn] VPN remote gateway and port not defined, defaulting to netherlands port 1194"
-			sed -i -e "s/remote\s.*/remote nl.privateinternetaccess.com 1194/g" "$VPN_CONFIG"
+			sed -i -e "s/remote\s.*/remote nl.privateinternetaccess.com 1194/g" "${VPN_CONFIG}"
 
 		# if no remote gateway but port defined then use netherlands and defined port
 		elif [[ -z "${VPN_REMOTE}" && ! -z "${VPN_PORT}" ]]; then
 			echo "[warn] VPN remote gateway not defined and port defined, defaulting to netherlands"
-			sed -i -e "s/remote\s.*/remote nl.privateinternetaccess.com $VPN_PORT/g" "$VPN_CONFIG"
+			sed -i -e "s/remote\s.*/remote nl.privateinternetaccess.com ${VPN_PORT}/g" "${VPN_CONFIG}"
 
 		# if remote gateway defined but port not defined then use default port
 		elif [[ ! -z "${VPN_REMOTE}" && -z "${VPN_PORT}" ]]; then
 			echo "[warn] VPN remote gateway defined but no port defined, defaulting to port 1194"
-			sed -i -e "s/remote\s.*/remote $VPN_REMOTE 1194/g" "$VPN_CONFIG"
+			sed -i -e "s/remote\s.*/remote ${VPN_REMOTE} 1194/g" "${VPN_CONFIG}"
 
 		# if remote gateway and port defined then use both
 		else
-			echo "[info] VPN provider remote gateway and port defined as $VPN_REMOTE $VPN_PORT"
-			sed -i -e "s/remote\s.*/remote $VPN_REMOTE $VPN_PORT/g" "$VPN_CONFIG"
+			echo "[info] VPN provider remote gateway and port defined as ${VPN_REMOTE} ${VPN_PORT}"
+			sed -i -e "s/remote\s.*/remote ${VPN_REMOTE} ${VPN_PORT}/g" "${VPN_CONFIG}"
 		fi
 
 		# store credentials in separate file for authentication
-		if ! $(grep -Fq "auth-user-pass credentials.conf" "$VPN_CONFIG"); then
-			sed -i -e 's/auth-user-pass.*/auth-user-pass credentials.conf/g' "$VPN_CONFIG"
+		if ! $(grep -Fq "auth-user-pass credentials.conf" "${VPN_CONFIG}"); then
+			sed -i -e 's/auth-user-pass.*/auth-user-pass credentials.conf/g' "${VPN_CONFIG}"
 		fi
 
 		# write vpn username to file
@@ -85,17 +85,17 @@ else
 		fi
 
 	# if custom vpn provider chosen then do NOT copy base config file
-	elif [[ $VPN_PROV == "custom" ]]; then
+	elif [[ "${VPN_PROV}" == "custom" ]]; then
 
-		echo "[info] VPN provider defined as $VPN_PROV"
+		echo "[info] VPN provider defined as ${VPN_PROV}"
 		
 		if [[ -z "${VPN_CONFIG}" ]]; then
 			echo "[crit] Missing OpenVPN configuration file in /config/openvpn/ (no files with an ovpn extension exist) please create and restart delugevpn" && exit 1
 		fi
 
 		# store credentials in separate file for authentication
-		if ! $(grep -Fq "auth-user-pass credentials.conf" "$VPN_CONFIG"); then
-			sed -i -e 's/auth-user-pass.*/auth-user-pass credentials.conf/g' "$VPN_CONFIG"
+		if ! $(grep -Fq "auth-user-pass credentials.conf" "${VPN_CONFIG}"); then
+			sed -i -e 's/auth-user-pass.*/auth-user-pass credentials.conf/g' "${VPN_CONFIG}"
 		fi
 
 		# write vpn username to file
@@ -114,27 +114,27 @@ else
 
 	# if provider none of the above then exit
 	else
-		echo "[crit] VPN provider $VPN_PROV not recognised, please specify airvpn, pia, or custom using env variable VPN_PROV" && exit 1
+		echo "[crit] VPN provider ${VPN_PROV} not recognised, please specify airvpn, pia, or custom using env variable VPN_PROV" && exit 1
 	fi
 
 	# customise ovpn file to ping tunnel every 5 mins
-	if ! $(grep -Fxq "ping 300" "$VPN_CONFIG"); then
-		sed -i '/remote\s.*/a ping 300' "$VPN_CONFIG"
+	if ! $(grep -Fxq "ping 300" "${VPN_CONFIG}"); then
+		sed -i '/remote\s.*/a ping 300' "${VPN_CONFIG}"
 	fi
 
 	# customise ovpn file to restart tunnel after 10 mins if no reply from ping (twice)
-	if ! $(grep -Fxq "ping-restart 600" "$VPN_CONFIG"); then
-		sed -i '/ping 300/a ping-restart 600' "$VPN_CONFIG"
+	if ! $(grep -Fxq "ping-restart 600" "${VPN_CONFIG}"); then
+		sed -i '/ping 300/a ping-restart 600' "${VPN_CONFIG}"
 	fi
 
 	# remove persist-tun from ovpn file if present, this allows reconnection to tunnel on disconnect
-	if $(grep -Fq "persist-tun" "$VPN_CONFIG"); then
-		sed -i '/persist-tun/d' "$VPN_CONFIG"
+	if $(grep -Fq "persist-tun" "${VPN_CONFIG}"); then
+		sed -i '/persist-tun/d' "${VPN_CONFIG}"
 	fi
 
 	# read port number and protocol from ovpn file (used to define iptables rule)
-	VPN_PORT=$(cat "$VPN_CONFIG" | grep -P -o -m 1 '^remote\s[^\r\n]+' | grep -P -o -m 1 '[\d]+$')
-	VPN_PROTOCOL=$(cat "$VPN_CONFIG" | grep -P -o -m 1 '(?<=proto\s)[^\r\n]+')
+	VPN_PORT=$(cat "${VPN_CONFIG}" | grep -P -o -m 1 '^remote\s[^\r\n]+' | grep -P -o -m 1 '[\d]+$')
+	VPN_PROTOCOL=$(cat "${VPN_CONFIG}" | grep -P -o -m 1 '(?<=proto\s)[^\r\n]+')
 
 	# set permissions to user nobody
 	chown -R nobody:users /config/openvpn
