bfad5c314812ac4da990a367c879e6b3ceec6b14
binhex <megalith01@gmail.com>
2016-05-04 22:46:36 +0100

    put in restart of openvpn if it exits

--- apps/root/openvpn.sh
+++ apps/root/openvpn.sh
@@ -2,5 +2,21 @@
 
 echo "[info] Starting OpenVPN..."
 
-# run openvpn to create tunnel
-/usr/bin/openvpn --cd /config/openvpn --config "$VPN_CONFIG" --mute-replay-warnings --keepalive 10 60
+# set sleep period for recheck (in mins)
+sleep_period="10"
+
+# loop and restart openvpn on exit
+while true; do
+
+	# run openvpn to create tunnel
+	/usr/bin/openvpn --cd /config/openvpn --config "$VPN_CONFIG" --mute-replay-warnings --keepalive 10 60
+
+	echo "[warn] VPN connection terminated"
+
+	# kill process openvpn
+	/usr/bin/pkill openvpn
+
+	echo "[warn] Restarting VPN connection in ${sleep_period} mins"
+	sleep "${sleep_period}"m
+
+done
