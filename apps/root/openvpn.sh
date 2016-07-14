2cd930186f2219198665347d4fd3e7148ec8272c
binhex <megalith01@gmail.com>
2016-07-14 15:10:02 +0100

    remove auth for airvpn users

--- apps/root/openvpn.sh
+++ apps/root/openvpn.sh
@@ -8,8 +8,17 @@ sleep_period="10"
 # loop and restart openvpn on exit
 while true; do
 
-	# run openvpn to create tunnel
-	/usr/bin/openvpn --cd /config/openvpn --config "$VPN_CONFIG" --remote "${VPN_REMOTE}" "${VPN_PORT}" --proto "${VPN_PROTOCOL}" --reneg-sec 0 --auth-user-pass credentials.conf --mute-replay-warnings --keepalive 10 60
+	if [[ "${VPN_PROV}" != "airvpn" ]]; then
+
+		# run openvpn to create tunnel
+		/usr/bin/openvpn --cd /config/openvpn --config "$VPN_CONFIG" --remote "${VPN_REMOTE}" "${VPN_PORT}" --proto "${VPN_PROTOCOL}" --reneg-sec 0 --auth-user-pass credentials.conf --mute-replay-warnings --keepalive 10 60
+
+	else
+
+		# run openvpn to create tunnel (airvpn uses certs for auth thus no --auth-user-pass)
+		/usr/bin/openvpn --cd /config/openvpn --config "$VPN_CONFIG" --remote "${VPN_REMOTE}" "${VPN_PORT}" --proto "${VPN_PROTOCOL}" --reneg-sec 0 --mute-replay-warnings --keepalive 10 60
+
+	fi
 
 	echo "[warn] VPN connection terminated"
 
