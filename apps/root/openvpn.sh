1ef88fe2b61d89781c83d30c9bc681698d1cfb44
binhex <megalith01@gmail.com>
2017-01-05 19:49:33 +0000

    put in sleep kill for downloader

--- apps/root/openvpn.sh
+++ apps/root/openvpn.sh
@@ -1,36 +1,48 @@
 #!/bin/bash
 
+# define common command lne parameters for openvpn
+openvpn_cli="/usr/bin/openvpn --cd /config/openvpn --config "${VPN_CONFIG}" ==daemon --remote "${VPN_REMOTE}" "${VPN_PORT}" --proto "${VPN_PROTOCOL}" --reneg-sec 0 --mute-replay-warnings --auth-nocache --keepalive 10 60"
+
+if [[ "${VPN_PROV}" == "pia" ]]; then
+
+	# add additional flags to pass credentials and ignore local-remote warnings
+	openvpn_cli="${openvpn_cli} --auth-user-pass credentials.conf --disable-occ"
+
+elif [[ "${VPN_PROV}" != "airvpn" ]]; then
+
+	# add additional flags to pass credentials
+	openvpn_cli="${openvpn_cli} --auth-user-pass credentials.conf"
+
+fi
+
 echo "[info] Starting OpenVPN..."
+
+# run openvpn to create tunnel (daemonized)
+eval "${openvpn_cli}"
 
 # set sleep period for recheck (in secs)
 sleep_period="30"
 
-# loop and restart openvpn on process termination (foreground blocking)
+# loop and restart openvpn on process termination
 while true; do
 
-	openvpn_cli="/usr/bin/openvpn --cd /config/openvpn --config "${VPN_CONFIG}" --remote "${VPN_REMOTE}" "${VPN_PORT}" --proto "${VPN_PROTOCOL}" --reneg-sec 0 --mute-replay-warnings --auth-nocache --keepalive 10 60"
+	# check if openvpn is running, if not then start and kill sleep process for downloader shell
+	if ! pgrep -f /usr/bin/openvpn > /dev/null; then
 
-	if [[ "${VPN_PROV}" == "pia" ]]; then
+		echo "[warn] OpenVPN process not running, restarting..."
 
-		# add additional flags to pass credentials and ignore local-remote warnings
-		openvpn_cli="${openvpn_cli} --auth-user-pass credentials.conf --disable-occ"
+		# run openvpn to create tunnel
+		eval "${openvpn_cli}"
 
-	elif [[ "${VPN_PROV}" != "airvpn" ]]; then
+		echo "[info] OpenVPN restarted, killing sleep process for downloader to force ip/port refresh..."
+		pkill -P $(</home/nobody/downloader.sleep.pid) sleep
+		echo "[info] Sleep process killed"
 
-		# add additional flags to pass credentials
-		openvpn_cli="${openvpn_cli} --auth-user-pass credentials.conf"
+		# sleep for 1 min to give openvpn chance to start before re-checking
+		sleep 1m
 
 	fi
 
-	# run openvpn to create tunnel
-	eval "${openvpn_cli}"
-
-	echo "[warn] VPN connection terminated"
-
-	# ensure openvpn process is dead
-	/usr/bin/pkill openvpn
-
-	echo "[warn] Restarting VPN connection in ${sleep_period} secs"
 	sleep "${sleep_period}"s
 
 done
