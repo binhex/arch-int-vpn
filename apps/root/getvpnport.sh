23311e3ea326c549fded632b66b9f8fd943528fd
binhex <megalith01@gmail.com>
2017-02-08 17:55:34 +0000

    remove ref to checkproc

--- apps/root/getvpnport.sh
+++ apps/root/getvpnport.sh
@@ -8,7 +8,7 @@ client_id=$(head -n 100 /dev/urandom | md5sum | tr -d " -")
 
 # get an assigned incoming port from pia's api using curl
 echo "[info] Attempting connection to PIA in order to assign a port forward for this session..."
-VPN_INCOMING_PORT=$(curl --connect-timeout 10 --max-time 20 --retry 6 --retry-max-time 120 -s "${pia_api_url}/?client_id=$client_id" | jq -r '.port')
+VPN_INCOMING_PORT=$(curl --connect-timeout 10 --max-time 20 --retry 6 --retry-max-time 120 -s "${pia_api_url}/?client_id=${client_id}" | jq -r '.port')
 
 if [[ ! "${VPN_INCOMING_PORT}" =~ ^-?[0-9]+$ ]]; then
   echo "[warn] Port forwarding is already activated on this connection, has expired, or you are NOT connected to a PIA region that supports port forwarding"
