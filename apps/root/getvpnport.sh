b2ea0e87c74eb85216cae4022446f95ee355328d
binhex <megalith01@gmail.com>
2017-02-11 23:19:10 +0000

    bg openvpn up script required

--- apps/root/getvpnport.sh
+++ apps/root/getvpnport.sh
@@ -4,15 +4,20 @@
 pia_api_url="http://209.222.18.222:2000"
 
 # create pia client id (randomly generated)
-client_id=$(head -n 100 /dev/urandom | md5sum | tr -d " -")
+client_id=$(head -n 100 /dev/urandom | sha256sum | tr -d " -")
+
+# sleep to allow tunnel to complete initialisation before we attempt to use curl to get port
+sleep 5s
 
 # get an assigned incoming port from pia's api using curl
 echo "[info] Attempting connection to PIA in order to assign a port forward for this session..."
 VPN_INCOMING_PORT=$(curl --connect-timeout 10 --max-time 20 --retry 6 --retry-max-time 120 -s "${pia_api_url}/?client_id=${client_id}" | jq -r '.port')
 
 if [[ ! "${VPN_INCOMING_PORT}" =~ ^-?[0-9]+$ ]]; then
-  echo "[warn] Port forwarding is already activated on this connection, has expired, or you are NOT connected to a PIA region that supports port forwarding"
-  VPN_INCOMING_PORT=""
+	echo "[warn] Port forwarding is already activated on this connection, has expired, or you are NOT connected to a PIA region that supports port forwarding"
+	VPN_INCOMING_PORT=""
+ else
+	echo "[info] Port forward successfully assigned port '${VPN_INCOMING_PORT}'"
 fi
 
 # write port number to text file, this is then read by the downloader script
--- apps/root/openvpn.sh
+++ apps/root/openvpn.sh
@@ -6,7 +6,7 @@ openvpn_cli="/usr/bin/openvpn --cd /config/openvpn --config ${VPN_CONFIG} --daem
 if [[ "${VPN_PROV}" == "pia" ]]; then
 
 	# add additional flags to pass credentials and ignore local-remote warnings
-	openvpn_cli="${openvpn_cli} --auth-user-pass credentials.conf --disable-occ --script-security 2 --up /root/getvpnport.sh --up-delay --up-restart"
+	openvpn_cli="${openvpn_cli} --auth-user-pass credentials.conf --disable-occ --script-security 2 --up \"/bin/bash /root/getvpnport.sh &\" --up-delay --up-restart"
 
 elif [[ "${VPN_PROV}" != "airvpn" ]]; then
 
@@ -34,31 +34,18 @@ echo "[info] OpenVPN started"
 # run script to check ip is valid for tunnel device (will block until valid)
 source /home/nobody/getvpnip.sh
 
-# define location and name of pid file
-pid_file="/home/nobody/downloader.sleep.pid"
-
 # set sleep period for recheck (in secs)
 sleep_period="30"
 
 # loop and restart openvpn on process termination
 while true; do
 
-	# check if openvpn is running, if not then restart and kill sleep process for downloader shell
+	# check if openvpn is running, if not then restart
 	if ! pgrep -x openvpn > /dev/null; then
 
 		echo "[warn] OpenVPN process terminated, restarting OpenVPN..."
 		eval "${openvpn_cli}"
 		echo "[info] OpenVPN restarted"
-
-		sleep "${sleep_period}"s
-
-		if [[ -f "${pid_file}" ]]; then
-
-			echo "[info] Killing sleep command in rtorrent.sh to force refresh of ip/port..."
-			pkill -P $(<"${pid_file}") sleep
-			echo "[info] Refresh process started"
-
-		fi
 
 	fi
 
