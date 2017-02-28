eb6f258e7eeca75a94be9a31ce5459476b2bf0f3
binhex <megalith01@gmail.com>
2017-02-28 18:06:21 +0000

    minor fixes and removal of logging for up scripts

--- apps/root/getvpnextip.sh
+++ apps/root/getvpnextip.sh
@@ -9,31 +9,20 @@ sec_external_ip_ns="resolver1.opendns.com"
 
 # use dns query to get external ip address
 external_ip="$(dig TXT +short o-o.myaddr.l.google.com @${pri_external_ip_ns} | tr -d '"')"
-check_valid_ip "${external_ip}"
 exit_code="${?}"
 
 # if error then try secondary name server
 if [[ "${exit_code}" != 0 ]]; then
 
-	echo "[warn] Cannot determine external IP address from '${pri_external_ip_ns}', trying alternative name server..."
-
 	external_ip="$(dig +short myip.opendns.com @${sec_external_ip_ns})"
-	check_valid_ip "${external_ip}"
 	exit_code="${?}"
 
 	if [[ "${exit_code}" != 0 ]]; then
 
-		echo "[warn] Cannot determine external IP address from '${sec_external_ip_ns}', possible connection issues at present."
-
 		external_ip="0.0.0.0"
-		return 1
 
 	fi
 
-fi
-
-if [[ "${DEBUG}" == "true" ]]; then
-	echo "[debug] External IP address from tunnel is '${external_ip}'"
 fi
 
 # write external ip address to text file, this is then read by the downloader script
--- apps/root/getvpnport.sh
+++ apps/root/getvpnport.sh
@@ -10,14 +10,10 @@ pia_api_url="http://209.222.18.222:2000"
 client_id=$(head -n 100 /dev/urandom | sha256sum | tr -d " -")
 
 # get an assigned incoming port from pia's api using curl
-echo "[info] Attempting connection to PIA in order to assign a port forward for this session..."
 VPN_INCOMING_PORT=$(curl --connect-timeout 5 --max-time 10 --retry 6 --retry-max-time 60 -s "${pia_api_url}/?client_id=${client_id}" | jq -r '.port')
 
 if [[ ! "${VPN_INCOMING_PORT}" =~ ^-?[0-9]+$ ]]; then
-	echo "[warn] Port forwarding is already activated on this connection, has expired, or you are NOT connected to a PIA region that supports port forwarding"
 	VPN_INCOMING_PORT=""
- else
-	echo "[info] Port forward successfully assigned port '${VPN_INCOMING_PORT}'"
 fi
 
 # write port number to text file, this is then read by the downloader script
