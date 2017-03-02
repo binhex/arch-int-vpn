b52ba5a3ea0de3ae2c1c53be3c6f62cdbd3da775
binhex <megalith01@gmail.com>
2017-03-02 13:08:39 +0000

    just poke google.com 443

--- apps/root/getvpnport.sh
+++ apps/root/getvpnport.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+# ensure we have connectivity before attempting to assign incoming port from pia api
+source /root/checkvpnconn.sh "google.com" "443"
+
 # statically assigned url for pia api (taken from pia script)
 pia_api_host="209.222.18.222"
 pia_api_port="2000"
 pia_api_url="http://${pia_api_host}:${pia_api_port}"
-
-# ensure we have connectivity before attempting to assign incoming port from pia api
-source /root/checkvpnconn.sh "${pia_api_host}" "${pia_api_port}"
 
 # ugly hack to wait until pia api is properly accessible (cnanot find a way to identify this state at the moment)
 sleep 5s
