c0009762205fd99e108456a331b04424216d7de6
binhex <megalith01@gmail.com>
2017-06-22 20:11:13 +0100

    quiet logging down

--- run/nobody/getvpnport.sh
+++ run/nobody/getvpnport.sh
@@ -2,10 +2,12 @@
 
 vpn_port="/home/nobody/vpn_incoming_port.txt"
 
-echo "[info] Waiting for file '${vpn_port}' to be generated (contains PIA API generated incoming port number)..."
 while [ ! -f "${vpn_port}" ]
 do
-	sleep 0.1s
+	if [[ "${DEBUG}" == "true" ]]; then
+		echo "[debug] Waiting for file '${vpn_port}' to be generated (contains PIA API generated incoming port number)..."
+	fi
+	sleep 1s
 done
 
 VPN_INCOMING_PORT=$(<"${vpn_port}")
