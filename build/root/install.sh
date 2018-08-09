50e3c6d0632c03b689a507ec040dc32995c55b76
binhex <megalith01@gmail.com>
2018-08-09 13:25:43 +0100

    switch openvpn to specific version of bind-tools (dig)

--- build/root/install.sh
+++ build/root/install.sh
@@ -15,11 +15,25 @@ unzip /tmp/scripts-master.zip -d /tmp
 # move shell scripts to /root
 mv /tmp/scripts-master/shell/arch/docker/*.sh /root/
 
+# archive packages
+####
+
+# define archive packages
+# note - the below installs a specific version of dig, required for synology
+# boxes which seem to crash out with the following message on the latest 
+# version of bind-tools (v9.13.0):-
+# random.c:102: fatal error: RUNTIME_CHECK(ret >= 0) failed
+
+arc_packages="bind-tools~9.12.1-1-x86_64"
+
+# call arc script (arch archive repo)
+source /root/arc.sh
+
 # pacman packages
 ####
 
 # define pacman packages
-pacman_packages="kmod openvpn privoxy dnsutils"
+pacman_packages="kmod openvpn privoxy"
 
 # install pre-reqs
 pacman -S --needed $pacman_packages --noconfirm
