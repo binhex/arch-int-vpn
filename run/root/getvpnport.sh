71b28657c6c7be1b44aad39dfaf1e37f4e1a1605
binhex <megalith01@gmail.com>
2019-01-24 14:47:09 +0000

    always log pf enabled endpoints

--- run/root/getvpnport.sh
+++ run/root/getvpnport.sh
@@ -45,26 +45,38 @@ if [[ "${VPN_PROV}" == "pia" ]]; then
 		/root/curly.sh -rc 12 -rw 10 -of "/tmp/pia_vpninfo_api_result" -url "${pia_vpninfo_api}"
 
 		if [[ "${?}" != 0 ]]; then
+
 			echo "[warn] PIA VPN info API currently down, skipping endoint port forwward check"
-		fi
 
-		# run jq query with the filter
-		jq_query_result=$(cat "/tmp/pia_vpninfo_api_result" | jq -r "${jq_query_filter_portforward}" 2> /dev/null)
+		else
 
-		# run jq query to get endpoint name (dns) only, use xargs to turn into single line string
-		jq_query_details=$(echo "${jq_query_result}" | jq -r '.dns' | xargs)
+			# run jq query with the filter
+			jq_query_result=$(cat "/tmp/pia_vpninfo_api_result" | jq -r "${jq_query_filter_portforward}" 2> /dev/null)
 
-		# run grep to check that defined vpn remote is in the list of port forward enabled endpoints
-		# grep -w = exact match (whole word), grep -q = quiet mode (no output)
-		echo "${jq_query_details}" | grep -qw "${VPN_REMOTE}"
-		
-		if [[ "${?}" != 0 ]]; then
+			# run jq query to get endpoint name (dns) only, use xargs to turn into single line string
+			jq_query_details=$(echo "${jq_query_result}" | jq -r '.dns' | xargs)
 
-			echo "[warn] PIA endpoint '${VPN_REMOTE}' is not in the list of endpoints that support port forwarding, DL/UL speeds maybe slow"
-			echo "[info] Please consider switching to one of the endpoints below that does support port forwarding:-"
+			# run grep to check that defined vpn remote is in the list of port forward enabled endpoints
+			# grep -w = exact match (whole word), grep -q = quiet mode (no output)
+			echo "${jq_query_details}" | grep -qw "${VPN_REMOTE}"
+
+			if [[ "${?}" != 0 ]]; then
+
+				echo "[warn] PIA endpoint '${VPN_REMOTE}' is not in the list of endpoints that support port forwarding, DL/UL speeds maybe slow"
+				echo "[info] Please consider switching to one of the endpoints shown below"
+
+			else
+
+				if [[ "${DEBUG}" == "true" ]]; then
+					echo "[debug] PIA endpoint '${VPN_REMOTE}' is in the list of endpoints that support port forwarding"
+				fi
+
+			fi
 
 			# convert to list with separator being space
 			IFS=' ' read -ra jq_query_details_list <<< "${jq_query_details}"
+
+			echo "[info] List of PIA endpoints that support port forwarding:-"
 
 			# loop over list of port forward enabled endpooints and echo out to console
 			for i in "${jq_query_details_list[@]}"; do
