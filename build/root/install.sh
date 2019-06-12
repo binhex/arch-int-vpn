24e9a430532c05e0c8581e0c3a718823f7f2c55c
binhex <megalith01@gmail.com>
2019-06-12 21:16:27 +0100

    remove orphan packages and config

--- build/root/install.sh
+++ build/root/install.sh
@@ -26,6 +26,7 @@ pacman -S --needed $pacman_packages --noconfirm
 
 # cleanup
 yes|pacman -Scc
+pacman --noconfirm -Rns $(pacman -Qtdq) 2> /dev/null || true
 rm -rf /usr/share/locale/*
 rm -rf /usr/share/man/*
 rm -rf /usr/share/gtk-doc/*
