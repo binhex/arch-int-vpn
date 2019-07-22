a6d4ab2411c32ce0df7986136f5cd9903870cb8b
binhex <megalith01@gmail.com>
2019-07-22 13:15:42 +0100

    bug fixes

--- run/nobody/checkextport.sh
+++ run/nobody/checkextport.sh
@@ -3,35 +3,47 @@
 # url to external site used to check incoming port is open
 incoming_port_check_url="https://portchecker.co/check"
 
-# make sure external website used to check incoming port is operational
-exit_code=$(curly.sh -rc 5 -rw 2 -sm true -url "${incoming_port_check_url}")
+if [[ -z "${rtorrent_port}" ]]; then
+	echo "[warn] rTorrent port is not defined" ; return 3
+fi
 
-if [[ "${exit_code}" -eq 0 ]]; then
+# make sure external website used to check incoming port is operational
+curly.sh -rc 5 -rw 2 -sm true -url "${incoming_port_check_url}"
+
+if [[ "${?}" -eq 0 ]]; then
 
 	if [[ "${DEBUG}" == "true" ]]; then
 		echo "[debug] Checking rTorrent incoming port '${rtorrent_port}' is open, using external website '${incoming_port_check_url}'..."
 	fi
 
-	# use curl to check incoming port is open
-	exit_code=$(curl --connect-timeout 30 --max-time 120 --silent --data "port=${rtorrent_port}&submit=Check" -X POST "${incoming_port_check_url}" | grep -q -i "port ${rtorrent_port} is.*open")
+	# use curl to check incoming port is open (web scrape)
+	curl --connect-timeout 30 --max-time 120 --silent --data "port=${rtorrent_port}&submit=Check" -X POST "${incoming_port_check_url}" | grep -i -P "port ${rtorrent_port} is" | grep -i 'open' 1> /dev/null
 
-	if [[ "${exit_code}" -ne 0 ]]; then
+	if [[ "${?}" -eq 0 ]]; then
 
-		echo "[info] rTorrent incoming port closed, marking for reconfigure"
+		if [[ "${DEBUG}" == "true" ]]; then
 
-		# mark as reconfigure required due to mismatch
-		port_change="true"
+			echo "[debug] rTorrent incoming port '${rtorrent_port}' is open"
+
+			# mark as port open by returning zero value
+			return 0
+
+		fi
 
 	else
 
-		if [[ "${DEBUG}" == "true" ]]; then
-			echo "[debug] rTorrent incoming port '${rtorrent_port}' is open"
-		fi
+		echo "[info] rTorrent incoming port closed, marking for reconfigure"
+
+		# mark for reconfigure by returning non zero value
+		return 1
 
 	fi
 
 else
 
 	echo "[warn] External site '${incoming_port_check_url}' used to check incoming port is currently down"
+
+	# mark as failure to connect to external site to check port by returning non zero value
+	return 2
 
 fi
