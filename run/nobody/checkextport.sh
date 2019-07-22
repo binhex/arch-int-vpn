f6fd725161a104831ac7b9432e52a9ec7b854241
binhex <megalith01@gmail.com>
2019-07-22 22:30:10 +0100

    add additional site and define and call function

--- run/nobody/checkextport.sh
+++ run/nobody/checkextport.sh
@@ -1,49 +1,76 @@
 #!/bin/bash
 
-# url to external site used to check incoming port is open
-incoming_port_check_url="https://portchecker.co/check"
-
 if [[ -z "${rtorrent_port}" ]]; then
 	echo "[warn] rTorrent port is not defined" ; return 3
 fi
 
-# make sure external website used to check incoming port is operational
-curly.sh -rc 5 -rw 2 -sm true -url "${incoming_port_check_url}"
+# function to check incoming port is open
+function check_incoming_port() {
 
-if [[ "${?}" -eq 0 ]]; then
+	incoming_port_check_url="${1}"
+	regex_open="${2}"
+	regex_closed="${3}"
 
-	if [[ "${DEBUG}" == "true" ]]; then
-		echo "[debug] Checking rTorrent incoming port '${rtorrent_port}' is open, using external website '${incoming_port_check_url}'..."
-	fi
-
-	# use curl to check incoming port is open (web scrape)
-	curl --connect-timeout 30 --max-time 120 --silent --data "port=${rtorrent_port}&submit=Check" -X POST "${incoming_port_check_url}" | grep -i -P "port ${rtorrent_port} is" | grep -i 'open' 1> /dev/null
+	# make sure external website used to check incoming port is operational
+	curly.sh -rc 5 -rw 2 -sm true -url "${incoming_port_check_url}"
 
 	if [[ "${?}" -eq 0 ]]; then
 
 		if [[ "${DEBUG}" == "true" ]]; then
+			echo "[debug] Checking rTorrent incoming port '${rtorrent_port}' is open, using external website '${incoming_port_check_url}'..."
+		fi
 
-			echo "[debug] rTorrent incoming port '${rtorrent_port}' is open"
+		# use curl to check incoming port is open (web scrape)
+		curl --connect-timeout 30 --max-time 120 --silent --data "port=${rtorrent_port}&submit=Check" -X POST "${incoming_port_check_url}" | grep -i -P "${regex_open}" 1> /dev/null
 
-			# mark as port open by returning zero value
-			return 0
+		if [[ "${?}" -eq 0 ]]; then
+
+			if [[ "${DEBUG}" == "true" ]]; then
+
+				echo "[debug] rTorrent incoming port '${rtorrent_port}' is open"
+
+				# mark as port open by returning zero value
+				return 0
+
+			fi
+
+		else
+
+			# if port is not open then check we have a match for closed, if no match then suspect web scrape issue
+			curl --connect-timeout 30 --max-time 120 --silent --data "port=${rtorrent_port}&submit=Check" -X POST "${incoming_port_check_url}" | grep -i -P "${regex_closed}" 1> /dev/null
+
+			if [[ "${?}" -eq 0 ]]; then
+
+				echo "[info] rTorrent incoming port closed, marking for reconfigure"
+
+				# mark for reconfigure by returning non zero value
+				return 1
+
+			else
+
+				echo "[warn] Incoming port site '${incoming_port_check_url}' failed to web scrape, marking as failed"
+
+				# mark as web scrape failed
+				return 4
+
+			fi
 
 		fi
 
 	else
 
-		echo "[info] rTorrent incoming port closed, marking for reconfigure"
+		echo "[warn] External site '${incoming_port_check_url}' used to check incoming port is currently down"
 
-		# mark for reconfigure by returning non zero value
-		return 1
+		# mark as failure to connect to external site to check port by returning non zero value
+		return 2
 
 	fi
+}
 
-else
+# run function for first site (web scrape)
+check_incoming_port "https://portchecker.co/check" "port ${rtorrent_port} is.*?open" "port ${rtorrent_port} is.*?closed"
 
-	echo "[warn] External site '${incoming_port_check_url}' used to check incoming port is currently down"
-
-	# mark as failure to connect to external site to check port by returning non zero value
-	return 2
-
+# if site down or web scrape error then run function for second site (web scrape)
+if [[ "${?}" -eq 2 || "${?}" -eq 4 ]]; then
+	check_incoming_port "https://canyouseeme.org/" "success.*?on port.*?${rtorrent_port}" "error.*?on port.*?${rtorrent_port}"
 fi
