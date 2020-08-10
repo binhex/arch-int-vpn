b09d2dca9a8f52bedf5cd695f0d505e6ebb57f6a
binhex <megalith01@gmail.com>
2020-08-10 13:28:44 +0100

    remove var as now using touch of file to indicate port closed

--- run/nobody/checkextport.sh
+++ run/nobody/checkextport.sh
@@ -39,7 +39,6 @@ function check_incoming_port() {
 				echo "[debug] ${APPLICATION} incoming port '${!application_incoming_port}' is open"
 
 				# mark as port open by returning zero value and setting variable
-				vpn_port_change="false"
 				set +x ; return 0
 
 			fi
@@ -54,7 +53,6 @@ function check_incoming_port() {
 				echo "[info] ${APPLICATION} incoming port closed, marking for reconfigure"
 
 				# mark for reconfigure by returning non zero value and setting variable
-				vpn_port_change="true"
 				set +x ; return 1
 
 			else
@@ -62,7 +60,6 @@ function check_incoming_port() {
 				echo "[warn] Incoming port site '${incoming_port_check_url}' failed to web scrape, marking as failed"
 
 				# mark as web scrape failed
-				vpn_port_change="false"
 				set +x ; return 4
 
 			fi
@@ -74,7 +71,6 @@ function check_incoming_port() {
 		echo "[warn] External site '${incoming_port_check_url}' used to check incoming port is currently down"
 
 		# mark as failure to connect to external site to check port by returning non zero value
-		vpn_port_change="false"
 		set +x ; return 2
 
 	fi
@@ -85,5 +81,15 @@ check_incoming_port "https://portchecker.co/" "target_ip=${external_ip}&port=${!
 
 # if site down or web scrape error then run function for second site (web scrape)
 if [[ "${?}" -eq 2 || "${?}" -eq 4 ]]; then
+
 	check_incoming_port "https://canyouseeme.org/" "port=${!application_incoming_port}&submit=Check" "success.*?on port.*?${!application_incoming_port}" "error.*?on port.*?${!application_incoming_port}"
+
+fi
+
+# if port not open or site down then create file to indicate failure which will
+# trigger a restart of openvpn by /root/openvpn.sh
+if [[ "${?}" -ne 0 ]]; then
+
+	touch "/tmp/portclosed"
+
 fi
