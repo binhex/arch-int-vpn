427be04fba06aa02448247d146c4943c39ec7cf2
Jesus Climent <climent@gmail.com>
2020-11-21 08:15:02 -0500

    Delete openvpn-specific files

--- run/root/openvpndown.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ "${VPN_PROV}" == "pia" ]]; then
-	if [ -f '/tmp/getvpnport.pid' ]; then
-		# kill getvpnport.sh on openvpn down, note use sig 15 not 2
-		kill -15 $(cat '/tmp/getvpnport.pid')
-	fi
-fi
--- run/root/openvpnup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# run scripts to get tunnel ip, check dns, get external ip, and get incoming port
-# note needs to be run in background, otherwise it blocks openvpn
-/root/prerunget.sh &
