f26c986265a6bd1c56f1b4d717e4f90fea84614a
binhex <megalith01@gmail.com>
2021-01-27 09:42:46 +0000

    silence kill error if no pid in file, also cleanup pid file

--- run/root/openvpndown.sh
+++ run/root/openvpndown.sh
@@ -3,6 +3,7 @@
 if [[ "${VPN_PROV}" == "pia" ]]; then
 	if [ -f '/tmp/getvpnport.pid' ]; then
 		# kill getvpnport.sh on openvpn down, note use sig 15 not 2
-		kill -15 $(cat '/tmp/getvpnport.pid')
+		kill -15 $(cat '/tmp/getvpnport.pid') 2> /dev/null
+		rm -f '/tmp/getvpnport.pid'
 	fi
 fi
--- run/root/wireguarddown.sh
+++ run/root/wireguarddown.sh
@@ -3,6 +3,7 @@
 if [[ "${VPN_PROV}" == "pia" ]]; then
 	if [ -f '/tmp/getvpnport.pid' ]; then
 		# kill getvpnport.sh on wireguard down, note use sig 15 not 2
-		kill -15 $(cat '/tmp/getvpnport.pid')
+		kill -15 $(cat '/tmp/getvpnport.pid') 2> /dev/null
+		rm -f '/tmp/getvpnport.pid'
 	fi
 fi
