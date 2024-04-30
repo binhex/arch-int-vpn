ac9feb1b16098bb4fcdfef9dd42e621c0922ef3a
binhex <megalith01@gmail.com>
2024-04-30 16:18:23 +0100

    fix return code issue with if

--- run/nobody/checkextport.sh
+++ run/nobody/checkextport.sh
@@ -11,15 +11,62 @@ if [[ -z "${external_ip}" ]]; then
 	echo "[warn] External IP address is not defined" ; return 4
 fi
 
+# function to check incoming port is open (webscrape)
+function check_incoming_port_webscrape() {
+
+	incoming_port_check_url="${1}"
+	post_data="${2}"
+	regex_open="${3}"
+	regex_closed="${4}"
+
+	site_up="false"
+	port_open="false"
+
+	if [[ "${DEBUG}" == "true" ]]; then
+		echo "[debug] Checking ${APPLICATION} incoming port '${!application_incoming_port}' is open, using external url '${incoming_port_check_url}'..."
+	fi
+
+	# use curl to check incoming port is open (web scrape)
+	if curl --connect-timeout 30 --max-time 120 --silent --data "${post_data}" -X POST "${incoming_port_check_url}" | grep -i -P "${regex_open}" 1> /dev/null; then
+
+		if [[ "${DEBUG}" == "true" ]]; then
+			echo "[debug] ${APPLICATION} incoming port '${!application_incoming_port}' is open"
+		fi
+		site_up="true"
+		port_open="true"
+		return
+
+	else
+
+		# if port is not open then check we have a match for closed, if no match then suspect web scrape issue
+		if curl --connect-timeout 30 --max-time 120 --silent --data "${post_data}" -X POST "${incoming_port_check_url}" | grep -i -P "${regex_closed}" 1> /dev/null; then
+
+			echo "[info] ${APPLICATION} incoming port closed, marking for reconfigure"
+			site_up="true"
+			return
+
+		else
+
+			echo "[warn] Incoming port site '${incoming_port_check_url}' failed to web scrape, marking as failed"
+			return
+
+		fi
+
+	fi
+
+}
+
 # function to check incoming port is open (json)
 function check_incoming_port_json() {
 
 	incoming_port_check_url="${1}"
 	json_query="${2}"
 
+	site_up="false"
+	port_open="false"
+
 	if [[ "${DEBUG}" == "true" ]]; then
 		echo "[debug] Checking ${APPLICATION} incoming port '${!application_incoming_port}' is open, using external url '${incoming_port_check_url}'..."
-		set -x
 	fi
 
 	response=$(curl --connect-timeout 30 --max-time 120 --silent "${incoming_port_check_url}" | jq "${json_query}")
@@ -29,61 +76,20 @@ function check_incoming_port_json() {
 		if [[ "${DEBUG}" == "true" ]]; then
 			echo "[debug] ${APPLICATION} incoming port '${!application_incoming_port}' is open"
 		fi
-		set +x ; return 0
+		site_up="true"
+		port_open="true"
+		return
 
 	elif [[ "${response}" == "false" ]]; then
 
 		echo "[info] ${APPLICATION} incoming port '${!application_incoming_port}' is closed, marking for reconfigure"
-		set +x ; return 1
+		site_up="true"
+		return
 
 	else
 
 		echo "[warn] Incoming port site '${incoming_port_check_url}' failed json download, marking as failed"
-		set +x ; return 2
-
-	fi
-
-}
-
-# function to check incoming port is open (webscrape)
-function check_incoming_port_webscrape() {
-
-	incoming_port_check_url="${1}"
-	post_data="${2}"
-	regex_open="${3}"
-	regex_closed="${4}"
-
-	if [[ "${DEBUG}" == "true" ]]; then
-		echo "[debug] Checking ${APPLICATION} incoming port '${!application_incoming_port}' is open, using external url '${incoming_port_check_url}'..."
-		set -x
-	fi
-
-	# use curl to check incoming port is open (web scrape)
-	curl --connect-timeout 30 --max-time 120 --silent --data "${post_data}" -X POST "${incoming_port_check_url}" | grep -i -P "${regex_open}" 1> /dev/null
-
-	if [[ "${?}" -eq 0 ]]; then
-
-		if [[ "${DEBUG}" == "true" ]]; then
-			echo "[debug] ${APPLICATION} incoming port '${!application_incoming_port}' is open"
-		fi
-		set +x ; return 0
-
-	else
-
-		# if port is not open then check we have a match for closed, if no match then suspect web scrape issue
-		curl --connect-timeout 30 --max-time 120 --silent --data "${post_data}" -X POST "${incoming_port_check_url}" | grep -i -P "${regex_closed}" 1> /dev/null
-
-		if [[ "${?}" -eq 0 ]]; then
-
-			echo "[info] ${APPLICATION} incoming port closed, marking for reconfigure"
-			set +x ; return 1
-
-		else
-
-			echo "[warn] Incoming port site '${incoming_port_check_url}' failed to web scrape, marking as failed"
-			set +x ; return 2
-
-		fi
+		return
 
 	fi
 
@@ -92,17 +98,13 @@ function check_incoming_port_webscrape() {
 # run function for first site (web scrape)
 check_incoming_port_webscrape "https://canyouseeme.org/" "port=${!application_incoming_port}&submit=Check" "success.*?on port.*?${!application_incoming_port}" "error.*?on port.*?${!application_incoming_port}"
 
-# if site down or web scrape error then run function for second site (json)
-if [[ "${?}" -eq 2 ]]; then
-
+# if web scrape error/site down then try second site (json)
+if [[ "${site_up}" == "false" ]]; then
 	check_incoming_port_json "https://ifconfig.co/port/${!application_incoming_port}" ".reachable"
-
 fi
 
-# if port not open or site down then create file to indicate failure which will
-# trigger a restart of openvpn by /root/openvpn.sh
-if [[ "${?}" -ne 0 ]]; then
-
+# if port down then mark as closed
+if [[ "${port_open}" == "false" ]]; then
 	touch "/tmp/portclosed"
-
+	return
 fi
