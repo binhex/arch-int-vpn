d7ae78c42a4b1856b18a5f300107e92856d3a356
binhex <megalith01@gmail.com>
2025-06-14 10:38:34 +0100

    remove build scripts, now part of base

--- build/root/install.sh
+++ build/root/install.sh
@@ -22,18 +22,6 @@ fi
 # write RELEASETAG to file to record the release tag used to build the image
 echo "INT_RELEASE_TAG=${RELEASETAG}" >> '/etc/image-release'
 
-# build scripts
-####
-
-# download build scripts from github
-curl --connect-timeout 5 --max-time 600 --retry 5 --retry-delay 0 --retry-max-time 60 -o /tmp/scripts-master.zip -L https://github.com/binhex/scripts/archive/master.zip
-
-# unzip build scripts
-unzip /tmp/scripts-master.zip -d /tmp
-
-# move shell scripts to /root
-mv /tmp/scripts-master/shell/arch/docker/*.sh /usr/local/bin/
-
 # pacman packages
 ####
 
